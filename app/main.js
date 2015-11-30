fb63f50551a236a0a002af267d6654a256eb3859
John Duhamel <john.duhamel@gmail.com>
2015-11-29 16:31:56 -0800

    Basic route is working

--- app/main.js
+++ app/main.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
-//import { Router, Route, Link } from 'react-router';
+import { Router, Route, Link } from 'react-router';
 
 import './styles/main.scss';
 
 import Counter from './components/Counter';
 import TodoList from './components/TodoList';
 
-class Hello extends React.Component {
+class App extends React.Component {
   render() {
-    return <h1>Hello {this.props.name}!</h1>
+    return (
+      <div id='app'>
+        <TodoList />
+      </div>
+    );
   }
 }
 
-render(<TodoList />, document.getElementById('content'));
+var routes = (
+  <Router>
+    <Route path='/' component={App}>
+    </Route>
+  </Router>
+);
+
+render(routes, document.getElementById('content'));
